75807044f2e7caae60a1076b6f68ca0bb219e140
RobeSantoro <santoro.robe@gmail.com>
2024-01-22 15:31:18 +0100

    Gsplat imported

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { Canvas } from '@react-three/fiber'
+
+import './App.css'
+import { OrbitControls } from '@react-three/drei'
+import Scene from './Scene'
+
+function App() {
+
+  return (
+
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'black',
+      }}
+    >
+
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        {/* <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="hotpink" />
+        </mesh> */}
+
+        <Scene />
+
+        <OrbitControls />
+
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Scene.tsx
@@ -0,0 +1,24 @@
+import { Object3DNode, extend } from '@react-three/fiber';
+import { LumaSplatsThree, LumaSplatsSemantics } from "@lumaai/luma-web";
+
+// Make LumaSplatsThree available to R3F
+extend( { LumaSplats: LumaSplatsThree } );
+
+// For typeScript support:
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    lumaSplats: Object3DNode<LumaSplatsThree, typeof LumaSplatsThree>
+  }
+}
+
+function Scene() {
+	return <lumaSplats
+		semanticsMask={LumaSplatsSemantics.FOREGROUND}
+		source='https://lumalabs.ai/capture/09e9f5a0-b9d3-492b-b35d-48bc026449e9'
+		position={[-1, 0, 0]}
+        rotation={[-0.5, 0, 0]}
+		scale={3}
+	/>
+}
+
+export default Scene
